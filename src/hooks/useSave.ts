99bd8e1139502cc9f7a4d97cd6a4be14e48f4c2f
lalli-oni <lalli.oni@gmail.com>
2024-11-22 12:06:43 +0100

    Update useSave hook Remove checkedState from hook props Change save effect to be an exported function Add load function Add encodeSaveId function

--- src/hooks/useSave.ts
+++ src/hooks/useSave.ts
@@ -1,13 +1,23 @@
 interface UseSaveProps {
 	cardId: number;
 	saveId: number;
-	checkedState: Array<number>;
 }
 
+const encodeSaveId = (cardId: string, saveId: string) => `${cardId.toString()}-${saveId.toString()}`;
+
 export const useSave = (props: UseSaveProps) => {
-	if (props.cardId && props.saveId && props.checkedState) {
-		localStorage.setItem(`${props.cardId.toString()}-${props.saveId.toString()}`, JSON.stringify(props.checkedState));
+	const load = () => {
+		const save = localStorage.getItem(encodeSaveId(props.cardId.toString(), props.saveId.toString()));
+		if (save === null) return save;
+		return JSON.parse(save);
 	}
 
-	return null;
+	const save = (cardState: Array<number>) => {
+		if (props.cardId && props.saveId !== undefined && cardState !== undefined) {
+			console.log('saving');
+			localStorage.setItem(encodeSaveId(props.cardId.toString(), props.saveId.toString()), JSON.stringify(cardState));
+		}
+	}
+
+	return { load, save };
 }
