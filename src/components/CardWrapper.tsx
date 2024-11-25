7b241dfc7c8c45ca5b43c4f5f765d68c9287ea80
lalli-oni <lalli.oni@gmail.com>
2024-11-25 14:23:10 +0100

    Select card in <CardWrapper />

--- src/components/CardWrapper.tsx
+++ src/components/CardWrapper.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import BingoCard from "./BingoCard";
+
+import cards from "../assets/cards.json";
 
 function CardWrapper() {
   const useQuery = () => new URLSearchParams(useLocation().search);
   const query = useQuery();
   const cardId = query.get("card");
 
-  if (cardId === undefined) return <div>Error reading card id!</div>;
+  if (!cardId) return <div>Error reading card id!</div>;
+
+  const cardIndex = parseInt(cardId) % cards.length; // Wrap around for invalid card IDs
+  const selectedCard = cards[cardIndex];
 
   return (
     <div className="App">
       <div className="card-container">
         <h1>Bingo Card nº {cardId}</h1>
-        <BingoCard cardId={cardId} />
+        {selectedCard ? <BingoCard card={selectedCard} cardIndex={cardIndex} /> : <div>Error loading card!</div>}
       </div>
     </div>);
 };
