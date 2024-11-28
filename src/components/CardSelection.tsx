e2fca91a6aaacedce4eb5622eb0cb144db627c95
lalli-oni <lalli.oni@gmail.com>
2024-11-28 11:23:10 +0100

    Style card selection Make auto sizing grid for select buttons

--- src/components/CardSelection.tsx
+++ src/components/CardSelection.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./CardSelection.css";
+
 export interface CardSelectionProps {
   onCardSelect: (cardIndex: number) => void;
 }
@@ -8,11 +10,18 @@ const cardIndexes = [...Array(61).keys()].slice(1);
 
 function CardSelection({ onCardSelect }: CardSelectionProps) {
   return (
-    <div>
-      <p>Please select a bingo card!</p>
-      <select onChange={(e) => onCardSelect(parseInt(e.target.value))}>
+    <div className="card-selection">
+      <p className="card-selection-title">Please select a bingo card!</p>
+      <div className="selection-container">
+        {cardIndexes.map((n) => 
+          (<input className="card-select" type="button" key={n}
+            value={n} onClick={(e) => onCardSelect(parseInt((e.target as HTMLInputElement).value))}
+          />)
+        )}
+      </div>
+      {/* <select onChange={(e) => onCardSelect(parseInt(e.target.value))}>
         {cardIndexes.map((n) => <option key={n} value={n}>{n}</option>)}
-      </select>
+      </select> */}
     </div>
   )
   
