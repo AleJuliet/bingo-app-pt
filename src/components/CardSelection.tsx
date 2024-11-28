183e91e7bc8198b7f450c77d366c8761751d976a
lalli-oni <lalli.oni@gmail.com>
2024-11-28 12:16:20 +0100

    Remove unused import

--- src/components/CardSelection.tsx
+++ src/components/CardSelection.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 import "./CardSelection.css";
-import { useSave } from "../hooks/useSave";
 
 export interface CardSelectionProps {
   onCardSelect: (cardIndex: number) => void;
