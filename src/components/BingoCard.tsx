e3a1a8df2413945b8d6cb007b5ca33d6d2eeae90
lalli-oni <lalli.oni@gmail.com>
2024-11-28 10:47:47 +0100

    Add error message

--- src/components/BingoCard.tsx
+++ src/components/BingoCard.tsx
@@ -19,10 +19,8 @@ const BingoCard = ({ card, cardIndex, onCardDeselect }: BingoCardProps) => {
   const [isLoading, setIsLoading] = useState(true);
   const ref = useRef(null);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [image, takeScreenShot] = useScreenshot({
-    type: "image/jpeg",
-    quality: 1.0
-  });
+  const [image, takeScreenShot] = useScreenshot();
+  const [error, setError] = useState<string>();
 
   const { load, save } = useSave({ cardId: cardIndex, saveId: 0, });
 
@@ -119,6 +117,12 @@ const BingoCard = ({ card, cardIndex, onCardDeselect }: BingoCardProps) => {
         {/*<button onClick={onCardDeselect}>Pick another card</button>*/
         }
       </div>
+      {error ?
+        <div className="error-message">
+          <span>{error}</span>
+          <input type="button" className="dismiss-button" onClick={() => setError('')} value="X" />
+        </div>
+      : null}
     </div>
   );
 };
